6b1c3a0e51707f19b9e25e13cadb8bf78303a0fe
wendelca <vvkamalov_obs@mail.ru>
2025-04-24 22:47:36 +0500

    init commit

---
+++ src/main/java/com/github/tennyros/solutions/SolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tennyros/solutions/SolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
